26f9d01197ed85a12e13f768830ae09bf5b1574c
xielulu <xiell3@asiainfo.com>
2019-12-23 09:43:16 +0800

    update

--- src/main/java/com/xll/invoke/InvokeTest.java
+++ src/main/java/com/xll/invoke/InvokeTest.java
@@ -3,6 +3,9 @@ package com.xll.invoke;
 import com.xll.entity.Human;
 import com.xll.entity.Man;
 
+/**
+ *  通过运行时类Class创建对象
+ */
 public class InvokeTest {
     public static void main(String[] args) throws IllegalAccessException, InstantiationException {
         Class<Man> clz = com.xll.entity.Man.class;
